9f062df05857aa35016db66eebdea6295fcb01cd
dokterfre <fredericsteegmans@gmail.com>
2024-11-06 15:36:58 +0100

    works goeie

--- ux-script.js
+++ ux-script.js
@@ -1,28 +1,13 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
+function openModal(imageSrc) {
+    const modal = document.getElementById("lightboxModal");
+    const modalImg = document.getElementById("lightboxImg");
+  
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    const modal = document.getElementById("lightboxModal");
+    modal.style.display = "none";
+  }
