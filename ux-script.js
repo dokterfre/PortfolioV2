6196f4df7092ca7670cc66d5654ea48484ca047b
dokterfre <fredericsteegmans@gmail.com>
2026-06-04 15:15:09 +0200

    optimize

--- ux-script.js
+++ ux-script.js
@@ -1,13 +1,1 @@
 
-function openModal(imageSrc) {
-    const modal = document.getElementById("lightboxModal");
-    const modalImg = document.getElementById("lightboxImg");
-  
-    modal.style.display = "block";
-    modalImg.src = imageSrc;
-  }
-
-  function closeModal() {
-    const modal = document.getElementById("lightboxModal");
-    modal.style.display = "none";
-  }
