e4ebd0b74bb535d30de816f82310bc9fb200355e
dokterfre <fredericsteegmans@gmail.com>
2026-06-04 15:24:11 +0200

    hj
    j

--- ux-script.js
+++ ux-script.js
@@ -1,1 +1,13 @@
+const videoObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const video = entry.target.querySelector('video');
+    if (!video) return;
+    if (entry.isIntersecting) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+}, { rootMargin: '100px' });
 
+document.querySelectorAll('.video-box').forEach(box => videoObserver.observe(box));
